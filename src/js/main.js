771b7547602a6fc776c4253d3e7144bab3e09aaa
Pavel3444 <burikov.pavel3444@yandex.ru>
2020-11-30 12:06:41 +0400

    add toggle animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,14 @@
 import './lib/lib';
 
 
+$('#first').on('click', () => {
+    $('div').eq(1).fadeToggle(800);
+});
 
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeToggle(800);
+});
 
-
-// $('button').click(function (){
-//     $('div').eq(1).hide();
-// })
-//
-// $('div').click(function (){
-//     console.log($(this).index())
-// })
-
-// console.log($('div').eq(2).find('.some'));
-
-// $('div').toggleAttribute('id', 'dssd')
-
-// console.log($('.some').closest('.findme'))
-// console.log($('.findme').siblings());
-// $('.findme').fadeOut(1800);
-// $('button').fadeIn(3800);
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeToggle(800);
+});
